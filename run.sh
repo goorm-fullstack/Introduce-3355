5238b0daf585e9f00cb0ea8decc421acf2c1dd59
WhiteKIM <9rudrb40@naver.com>
2023-07-03 14:29:36 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 echo ${SECURE_KEY} >> env.json
-sed 's/*/"/g' env.json >> ./src/resources/Key/testKey.json
+sed 's/*/"/g' env.json >> testKey.json
 java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/*.jar
