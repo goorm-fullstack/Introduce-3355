c11823bbb99fa3643e6be6b271dee1ff2db8a844
WhiteKIM <9rudrb40@naver.com>
2023-07-03 12:50:17 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-echo ${SECURE_FILE} >> env.json
+echo ${SECURE_KEY} >> env.json
 sed 's/*/"/g' env.json >> ./src/resources/Key/secureFile.json
 java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=prod -jar ./build/libs/dive-log-0.0.1-SNAPSHOT.jar
