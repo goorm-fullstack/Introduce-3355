5afb647999946bc0dbc50887c786aba67a22ca4a
WhiteKIM <9rudrb40@naver.com>
2023-07-03 14:57:24 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 echo ${SECURE_KEY} >> ./src/main/resources/Key/testKey.json
-java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=prod -jar ./build/libs/*.jar
+ java -Dserver.port=$PORT $JAVA_OPTS -jar target/Introduce-0.0.1.jar
