0bcebc67a6123b42707736a567e5cbf28f8fd0bb
WhiteKIM <9rudrb40@naver.com>
2023-07-03 17:36:01 +0900

    2023.07.03

--- src/main/java/Goorm/Introduce/Domain/FireBase/FirebaseInitialize.java
+++ src/main/java/Goorm/Introduce/Domain/FireBase/FirebaseInitialize.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
@@ -13,27 +14,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-@Service
 @RequiredArgsConstructor
 public class FirebaseInitialize {
-    private final ResourceLoader resourceLoader;
 
-    public static final String DATABASE_URL = "https://test-de4c9.firebaseio.com";//추후 연결된 DB변경시 변경
-
-    @PostConstruct
-    public void initialize() throws IOException {
-        Resource resource = resourceLoader.getResource("classpath:/Key/testKey.json");
-        try {
-            FileInputStream input = new FileInputStream(resource.getURI().getPath());
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(input))
-                    .setDatabaseUrl(DATABASE_URL)
-                    .build();
-            FirebaseApp.initializeApp(options);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
--- src/main/java/Goorm/Introduce/IntroduceApplication.java
+++ src/main/java/Goorm/Introduce/IntroduceApplication.java
@@ -1,12 +1,35 @@
 package Goorm.Introduce;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.*;
 
 @SpringBootApplication
 public class IntroduceApplication {
-
+	private static final ResourceLoader resourceLoader = new DefaultResourceLoader();
+	public static final String DATABASE_URL = "https://test-de4c9.firebaseio.com";//추후 연결된 DB변경시 변경
 	public static void main(String[] args) {
+		Resource resource = new ClassPathResource("Key/testKey.json");
+			try {
+				InputStream input = resource.getInputStream();
+				FirebaseOptions options = new FirebaseOptions.Builder()
+						.setCredentials(GoogleCredentials.fromStream(input))
+						.setDatabaseUrl(DATABASE_URL)
+						.build();
+				FirebaseApp.initializeApp(options);
+			} catch (FileNotFoundException e) {
+				throw new RuntimeException(e);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
 		SpringApplication.run(IntroduceApplication.class, args);
 	}
 
