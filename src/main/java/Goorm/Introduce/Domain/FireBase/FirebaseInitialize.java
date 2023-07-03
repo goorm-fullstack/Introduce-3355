305a2113627b59c508857ffdcf694142438d6860
WhiteKIM <9rudrb40@naver.com>
2023-07-03 17:21:21 +0900

    Update FirebaseInitialize.java

--- src/main/java/Goorm/Introduce/Domain/FireBase/FirebaseInitialize.java
+++ src/main/java/Goorm/Introduce/Domain/FireBase/FirebaseInitialize.java
@@ -13,27 +13,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-@Service
-@RequiredArgsConstructor
 public class FirebaseInitialize {
     private final ResourceLoader resourceLoader;
 
     public static final String DATABASE_URL = "https://test-de4c9.firebaseio.com";//추후 연결된 DB변경시 변경
 
-    @PostConstruct
-    public void initialize() throws IOException {
-        Resource resource = resourceLoader.getResource("classpath:/Key/testKey.json");
-        try {
-            FileInputStream input = new FileInputStream(resource.getURI().getPath());
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(input))
-                    .setDatabaseUrl(DATABASE_URL)
-                    .build();
-            FirebaseApp.initializeApp(options);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
