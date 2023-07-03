0d6718b1360864ce8f8ee9a0109bed3147e24c1b
WhiteKIM <9rudrb40@naver.com>
2023-07-03 14:45:29 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-echo ${SECURE_KEY} >> env.json
-sed 's/*/"/g' env.json >> ./src/main/resources/Key/testKey.json
+echo ${SECURE_KEY} >> ./src/main/resources/Key/testKey.json
 java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=prod -jar ./build/libs/*.jar
